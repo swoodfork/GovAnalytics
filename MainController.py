05d917650a6352228f84f7ac3c5994f029bd6219
swoodfork <48300645+swoodfork@users.noreply.github.com>
2019-04-07 10:48:51 -0400

    Update MainController and add MemberDirectory.xml

--- MainController.py
+++ MainController.py
@@ -1,48 +1,13 @@
-import xml.etree.ElementTree as eTree
-from GovAnalytics.entities import *
+from GovAnalytics.processors.XmlProcessor import XmlProcessor
+from GovAnalytics.processors.SqlProcessor import SqlProcessor
 
-tree = eTree.parse('Data.xml')
-root = tree.getroot()
+path = "D:\CongressData\congress\data"
 
-AllBills = []
+processor = SqlProcessor()
 
+all_bills = XmlProcessor.get_all_bills(path)
 
-print(root.tag)
+for bill in all_bills:
+    processor.insert_bill(bill)
 
-current = Bill(root.attrib["session"], root.attrib["type"], root.attrib["number"], root.attrib["updated"])
-
-
-e_map = {}
-index = 0
-
-
-
-
-
-
-for x in root:
-    if x.tag:
-        e_map[x.tag] = index
-        index += 1
-        print(x.tag)
-
-if e_map.__contains__("actions"):
-    for x in root[e_map["actions"]]:
-        if x.tag == "action":
-            text = ""
-            ref = ""
-            label = ""
-
-            for y in x:
-                if y.text:
-                    text = y.text
-                if y.attrib:
-                    ref = y.attrib["ref"]
-                    label = y.attrib["label"]
-
-            action = Action(x.attrib["datetime"], text, ref, label)
-            current.actions.append(action)
-            print(x.tag)
-
-print('\n\n')
-
+print("Total Number of Bills Processed: " + str(len(all_bills)))
