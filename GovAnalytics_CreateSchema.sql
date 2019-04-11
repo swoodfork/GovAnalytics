ae3977f7c9e06a738e231f93f1f7a91409a47a89
kar4xe <kar4xe@virginia.edu>
2019-04-10 19:13:15 -0500

    Add files via upload
    Added History Table creation and view definition

--- GovAnalytics_CreateSchema.sql
+++ GovAnalytics_CreateSchema.sql
@@ -1,11 +1,10 @@
 /*
-Version 1.0 - Mod: Spencer Woodfork
+Version 1.1 - Mod: Spencer Woodfork
+Version 1.2 - Mod: Katie Ryan
 */
 
 /*
-
 TABLE CREATION
-
 */
 
 
@@ -68,11 +67,72 @@ CREATE TABLE action(
         ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
+CREATE TABLE History (
+	hist_id INT NOT NULL AUTO_INCREMENT,
+	yr INT,
+    house varchar(20),
+	Pres_Party VARCHAR(20),
+    Majority_Party VARCHAR(20),
+    D_Net_Change INT,
+    R_Net_Change INT,
+    D_Total_Seat INT,
+    R_Total_Seat INT,
+    Ind_Total_Seat INT,
+    PRIMARY KEY (hist_id)
+);
 
 /*
+VIEW CREATION
+*/
 
+Create View bill_Summary
+as
+SELECT b.bill_id
+, b.session
+, b.type
+, b.number
+, case trim(party) when "Republican" then 1 else 0 end as Spons_Rep_ind
+, case trim(party) when "Democratic" then 1 else 0 end as Spons_Dem_ind
+, case trim(party) when "Independent" then 1 else 0 end as Indep_ind
+, case when st.state 
+in ("ENACTED:SIGNED","PASSED:BILL","PASSED:CONCURRENTRES","PASSED:SIMPLERES","") 
+then 1 else 0 end as Pass_Ind
+, st.state 
+, st.date
+
+, case when trim(party) = "Republican" AND h1.Pres_Party = "Republican" then 1
+when trim(party) = "Democratic" AND h1.Pres_Party = "Democrat" then 1 
+else 0 end as Pres_Party_Match
+, case when trim(party) = "Republican" AND h1.Majority_Party = "Republican" then 1
+when trim(party) = "Democratic" AND h1.Majority_Party = "Democrat" then 1 
+else 0 end as Senate_Party_Match
+, case when trim(party) = "Republican" then h1.R_Total_Seat
+when trim(party) = "Democratic" then h1.D_Total_Seat
+when trim(party) = "Independent" then h1.Ind_Total_Seat
+else null end as Num_Senators_In_Same_Party
+, case when trim(party) = "Republican" AND h2.Majority_Party = "Republican" then 1
+when trim(party) = "Democratic" AND h2.Majority_Party = "Democrat" then 1 
+else 0 end as HouseOfR_Party_Match
+, case when trim(party) = "Republican" then h2.R_Total_Seat
+when trim(party) = "Democratic" then h2.D_Total_Seat
+when trim(party) = "Independent" then h2.Ind_Total_Seat
+else null end as Num_HouseOfR_In_Same_Party
+
+FROM bill b
+inner join sponsor s 
+on b.bill_id = s.bill_id
+inner join state st 
+on b.bill_id = st.bill_id
+inner join history h1
+on h1.yr = year(st.date)
+and h1.house = "Senate"
+inner join history h2
+on h2.yr = year(st.date)
+and h2.house = "House of Rep"
+;
+
+/*
 STORED PROCEDURE CREATION
-
 */
 
 # Switch delimiter to //, so query will execute line by line.
@@ -113,19 +173,15 @@ BEGIN
 END
 
 /*
-
 DROP STATEMENTS:
-
 DROP TABLE action;
 DROP TABLE cosponsor;
 DROP TABLE sponsor;
 DROP TABLE STATE;
 DROP TABLE BILL;
-
 DROP PROCEDURE insert_action;
 DROP PROCEDURE insert_bill;
 DROP PROCEDURE  insert_cosponsor;
 DROP PROCEDURE insert_sponsor;
 DROP PROCEDURE insert_state;
-
 */
