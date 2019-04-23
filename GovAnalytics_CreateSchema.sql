418bb45a93281384b17910130f960058c69a3f68
swoodfork <48300645+swoodfork@users.noreply.github.com>
2019-04-23 19:00:39 -0400

    Add files via upload

--- GovAnalytics_CreateSchema.sql
+++ GovAnalytics_CreateSchema.sql
@@ -1,6 +1,7 @@
 /*
-Version 1.0 - 1.2 - Mod: Spencer Woodfork
+Version 1.0 - Mod: Spencer Woodfork
 Version 1.1 - Mod: Katie Ryan
+Version 1.2 - Mod: Spencer Woodfork - Added optimization improvements
 */
 
 /*
@@ -8,6 +9,7 @@ Version 1.1 - Mod: Katie Ryan
 TABLE CREATION
 
 */
+DROP TABLE IF EXISTS history;
 
 CREATE TABLE bill (
 	bill_id INT NOT NULL AUTO_INCREMENT,
@@ -92,62 +94,52 @@ CREATE TABLE summary(
 	bill_id INT,
     date DATE,
     status VARCHAR(100),
-    text TEXT,
+    text MEDIUMTEXT,
     INDEX (bill_id),
     FOREIGN KEY (bill_id)
 		REFERENCES bill(bill_id)
         ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
+CREATE TABLE History (
+	Year INT,
+	Pres_Party VARCHAR(20),
+    Majority_House_Party VARCHAR(20),
+    House_D_Net_Change INT,
+    House_R_Net_Change INT,
+    House_D_Total_Seat INT,
+    House_R_Total_Seat INT,
+    House_Ind_Total_Seat INT,
+	Majority_Senate_Party VARCHAR(20),
+	Senate_D_Net_Change INT,
+    Senate_R_Net_Change INT,
+    Senate_D_Total_Seat INT,
+    Senate_R_Total_Seat INT,
+    Senate_Ind_Total_Seat INT,
+    PRIMARY KEY (Year)
+);
+
 /*
 VIEW CREATION
 */
 
-Create View bill_Summary
-as
-SELECT b.bill_id
-, b.session
-, b.type
-, b.number
-, case trim(party) when "Republican" then 1 else 0 end as Spons_Rep_ind
-, case trim(party) when "Democratic" then 1 else 0 end as Spons_Dem_ind
-, case trim(party) when "Independent" then 1 else 0 end as Indep_ind
-, case when st.state
-in ("ENACTED:SIGNED","PASSED:BILL","PASSED:CONCURRENTRES","PASSED:SIMPLERES","")
-then 1 else 0 end as Pass_Ind
-, st.state
-, st.date
+DROP VIEW IF EXISTS bill_summary;
 
-, case when trim(party) = "Republican" AND h1.Pres_Party = "Republican" then 1
-when trim(party) = "Democratic" AND h1.Pres_Party = "Democrat" then 1
-else 0 end as Pres_Party_Match
-, case when trim(party) = "Republican" AND h1.Majority_Party = "Republican" then 1
-when trim(party) = "Democratic" AND h1.Majority_Party = "Democrat" then 1
-else 0 end as Senate_Party_Match
-, case when trim(party) = "Republican" then h1.R_Total_Seat
-when trim(party) = "Democratic" then h1.D_Total_Seat
-when trim(party) = "Independent" then h1.Ind_Total_Seat
-else null end as Num_Senators_In_Same_Party
-, case when trim(party) = "Republican" AND h2.Majority_Party = "Republican" then 1
-when trim(party) = "Democratic" AND h2.Majority_Party = "Democrat" then 1
-else 0 end as HouseOfR_Party_Match
-, case when trim(party) = "Republican" then h2.R_Total_Seat
-when trim(party) = "Democratic" then h2.D_Total_Seat
-when trim(party) = "Independent" then h2.Ind_Total_Seat
-else null end as Num_HouseOfR_In_Same_Party
-
+CREATE VIEW bill_summary
+AS
+SELECT
+	b.bill_id,
+	b.session,
+	b.type,
+	b.number,
+	s.party,
+    st.state,
+    st.date,
+    hist.*
 FROM bill b
-inner join sponsor s
-on b.bill_id = s.bill_id
-inner join state st
-on b.bill_id = st.bill_id
-inner join history h1
-on h1.yr = year(st.date)
-and h1.house = "Senate"
-inner join history h2
-on h2.yr = year(st.date)
-and h2.house = "House of Rep"
-;
+inner join sponsor s on b.bill_id = s.bill_id
+inner join state st on b.bill_id = st.bill_id
+inner join history hist on hist.year = year(st.date);
 
 /*
 
@@ -155,77 +147,137 @@ STORED PROCEDURE CREATION
 
 */
 
-# Switch delimiter to //, so query will execute line by line.
+DROP PROCEDURE IF EXISTS insert_subject;
+DROP PROCEDURE IF EXISTS insert_title;
+DROP PROCEDURE IF EXISTS insert_action;
+DROP PROCEDURE IF EXISTS insert_bill;
+DROP PROCEDURE IF EXISTS insert_cosponsor;
+DROP PROCEDURE IF EXISTS insert_sponsor;
+DROP PROCEDURE IF EXISTS insert_state;
+DROP PROCEDURE IF EXISTS insert_summary;
+
 DELIMITER //
 CREATE PROCEDURE insert_bill (IN session INT, IN type VARCHAR(100), IN number INT, IN updated DATE)
 BEGIN
 	INSERT INTO bill (session, type, number, updated) values (session, type, number, updated);
     SELECT MAX(bill_ID) FROM BILL;
-END
+END;
 
-# Switch delimiter to //, so query will execute line by line.
-DELIMITER //
 CREATE PROCEDURE insert_state (IN bill_id INT, IN state VARCHAR(100), IN date DATE)
 BEGIN
 	INSERT INTO state (bill_id, state, date) values (bill_id, state, date);
-END
+END;
 
-# Switch delimiter to //, so query will execute line by line.
-DELIMITER //
 CREATE PROCEDURE insert_action (IN bill_id INT, IN date DATE, IN text VARCHAR(5000), IN ref VARCHAR(100), IN label VARCHAR(100), IN how VARCHAR(100),
 	IN type VARCHAR(100), IN location VARCHAR(100), IN result VARCHAR(100), IN state VARCHAR(100))
 BEGIN
 	INSERT INTO action (bill_id, date, text, ref, label, how, type, location, result, state) values (bill_id, date, text, ref, label, how, type, location, result, state);
-END
+END;
 
-# Switch delimiter to //, so query will execute line by line.
-DELIMITER //
 CREATE PROCEDURE insert_sponsor (IN bill_id INT, IN name VARCHAR(100), IN party VARCHAR(100), IN state VARCHAR(100))
 BEGIN
 	INSERT INTO sponsor (bill_id, name, party, state) values (bill_id, name, party, state);
-END
+END;
 
-# Switch delimiter to //, so query will execute line by line.
-DELIMITER //
 CREATE PROCEDURE insert_cosponsor (IN bill_id INT, IN name VARCHAR(100), IN party VARCHAR(100), IN state VARCHAR(100), IN joined DATE)
 BEGIN
 	INSERT INTO cosponsor (bill_id, name, party, state, joined) values (bill_id, name, party, state, joined);
-END
+END;
 
-# Switch delimiter to //, so query will execute line by line.
-DELIMITER //
 CREATE PROCEDURE insert_title (IN bill_id INT, in type VARCHAR(100), IN title_as VARCHAR(100), IN text VARCHAR(2500))
 BEGIN
 	INSERT INTO title (bill_id, type, title_as, text) values (bill_id, type, title_as, text);
-END
+END;
 
-# Switch delimiter to //, so query will execute line by line.
-DELIMITER //
 CREATE PROCEDURE insert_subject (IN bill_id INT, IN name VARCHAR(1000))
 BEGIN
 	INSERT INTO subject (bill_id, name) values (bill_id, name);
-END
+END;
 
-# Switch delimiter to //, so query will execute line by line.
-DELIMITER //
-CREATE PROCEDURE insert_summary (IN bill_id INT, IN date DATE, IN status VARCHAR(100), IN text TEXT)
+CREATE PROCEDURE insert_summary (IN bill_id INT, IN date DATE, IN status VARCHAR(100), IN text MEDIUMTEXT)
 BEGIN
 	INSERT INTO summary (bill_id, date, status, text) values (bill_id, date, status, text);
-END
+END;
+
+/*
+DEFAULT DATA:
+*/
+
+INSERT INTO history
+(	year,
+    Pres_Party,
+    Majority_House_Party,
+    House_D_Net_Change,
+    House_R_Net_Change,
+    House_D_Total_Seat,
+    House_R_Total_Seat,
+    House_Ind_Total_Seat,
+	Majority_Senate_Party,
+    Senate_D_Net_Change,
+    Senate_R_Net_Change,
+    Senate_D_Total_Seat,
+    Senate_R_Total_Seat,
+    Senate_Ind_Total_Seat
+)
+VALUES
+(1976, 'Republican', 'Democrat', 1,-1,292,143,0, 'Democrat', 0,1,61,38,1),
+(1977, 'Republican', 'Democrat', 0,0,292,143,0, 'Democrat', 0,0,61,38,1),
+(1978, 'Democrat', 'Democrat', -15,1,277,158,0, 'Democrat', -2,2,59,40,1),
+(1979, 'Democrat', 'Democrat', 0,0,277,158,0, 'Democrat', -2,2,59,40,1),
+(1980, 'Democrat', 'Democrat', -34,34,243,192,0, 'Republican', -12,12,46,53,1),
+(1981, 'Democrat', 'Democrat', 0,0,243,192,0, 'Republican', 0,0,46,53,1),
+(1982, 'Republican', 'Democrat', 26,-26,269,166,0, 'Republican', 1,0,46,54,0),
+(1983, 'Republican', 'Democrat', 0,0,269,166,0, 'Republican', 0,0,46,54,0),
+(1984, 'Republican', 'Democrat', -16,16,253,182,0, 'Republican', 2,-2,47,53,0),
+(1985, 'Republican', 'Democrat', 0,0,253,182,0, 'Republican', 0,0,47,53,0),
+(1986, 'Republican', 'Democrat', 5,-5,258,177,0, 'Democrat', 8,-8,55,45,0),
+(1987, 'Republican', 'Democrat', 0,0,258,177,0, 'Democrat', 0,0,55,45,0),
+(1988, 'Republican', 'Democrat', 2,-2,260,175,0, 'Democrat', 1,-1,55,45,0),
+(1989, 'Republican', 'Democrat', 0,0,260,175,0, 'Democrat', 0,0,55,45,0),
+(1990, 'Republican', 'Democrat', 7,-8,267,167,1, 'Democrat', 1,-1,56,44,0),
+(1991, 'Republican', 'Democrat', 0,0,267,167,1, 'Democrat', 0,0,56,44,0),
+(1992, 'Republican', 'Democrat', -9,9,258,176,1, 'Democrat', -1,1,56,44,0),
+(1993, 'Republican', 'Democrat', 0,0,258,176,1, 'Democrat', 0,0,56,44,0),
+(1994, 'Democrat', 'Republican', -54,54,204,230,1, 'Republican', -8,8,48,52,0),
+(1995, 'Democrat', 'Republican', 0,0,204,230,1, 'Republican', 0,0,48,52,0),
+(1996, 'Democrat', 'Republican', 2,-3,207,226,2, 'Republican', -2,2,45,55,0),
+(1997, 'Democrat', 'Republican', 0,0,207,226,2, 'Republican', 0,0,45,55,0),
+(1998, 'Democrat', 'Republican', 5,-4,211,223,1, 'Republican', 0,0,45,55,0),
+(1999, 'Democrat', 'Republican', 0,0,211,223,1, 'Republican', 0,0,45,55,0),
+(2000, 'Democrat', 'Republican', 1,-2,212,221,2, 'Democrat', 4,-4,50,50,0),
+(2001, 'Democrat', 'Republican', 0,0,212,221,2, 'Democrat', 0,0,50,50,0),
+(2002, 'Republican', 'Republican', -7,8,205,229,1, 'Republican', -1,2,48,51,1),
+(2003, 'Republican', 'Republican', 0,0,205,229,1, 'Republican', 0,0,48,51,1),
+(2004, 'Republican', 'Republican', -3,3,202,232,1, 'Republican', -4,4,44,55,1),
+(2005, 'Republican', 'Republican', 0,0,202,232,1, 'Republican', 0,0,44,55,1),
+(2006, 'Republican', 'Democrat', 32,-27,233,202,0, 'Democrat', 5,-6,49,49,2),
+(2007, 'Republican', 'Democrat', 0,0,233,202,0, 'Democrat', 0,0,49,49,2),
+(2008, 'Republican', 'Democrat', 21,-21,257,178,0, 'Democrat', 8,-8,57,41,2),
+(2009, 'Republican', 'Democrat', 0,0,257,178,0, 'Democrat', 0,0,57,41,2),
+(2010, 'Democrat', 'Republican', -63,63,193,242,0, 'Democrat', -6,6,51,47,2),
+(2011, 'Democrat', 'Republican', 0,0,193,242,0, 'Democrat', 0,0,51,47,2),
+(2012, 'Democrat', 'Republican', 8,-8,201,234,0, 'Democrat', 2,-2,53,45,2),
+(2013, 'Democrat', 'Republican', 0,0,201,234,0, 'Democrat', 0,0,53,45,2),
+(2014, 'Democrat', 'Republican', -13,13,188,247,0, 'Republican', 9,-9,44,54,2),
+(2015, 'Democrat', 'Republican', 0,0,194,241,0, 'Republican', 0,0,44,54,2),
+(2016, 'Democrat', 'Republican', -6,6,194,241,0, 'Republican', 2,-2,46,52,2),
+(2017, 'Democrat', 'Republican', 0,0,194,241,0, 'Republican', 0,0,46,52,2),
+(2018, 'Republican', 'Democrat', 41,-42,235,199,1, 'Republican', -2,2,45,53,2);
 
 
 /*
 
 DROP STATEMENTS:
 
-DROP TABLE action;
-DROP TABLE cosponsor;
-DROP TABLE sponsor;
-DROP TABLE state;
-DROP TABLE title;
-DROP TABLE subject;
-DROP TABLE summary;
-DROP TABLE bill;
+DROP TABLE IF EXISTS history;
+DROP TABLE IF EXISTS action;
+DROP TABLE IF EXISTS cosponsor;
+DROP TABLE IF EXISTS sponsor;
+DROP TABLE IF EXISTS state;
+DROP TABLE IF EXISTS title;
+DROP TABLE IF EXISTS subject;
+DROP TABLE IF EXISTS summary;
+DROP TABLE IF EXISTS bill;
 
 
 DROP PROCEDURE insert_subject;
